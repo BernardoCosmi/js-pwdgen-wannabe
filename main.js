bec35678202a80cfefb8651fcde28fbdc7435df0
BernardoCosmi <bernardo.daddo90@gmail.com>
2023-12-21 15:33:42 +0100

    base-version complete

---
+++ main.js
@@ -0,0 +1,17 @@
+
+//nome
+let userName = prompt ('Inserisci il tuo nome');
+
+//cognome
+let userSurname = prompt ('Inserisci il tuo cognome');
+
+//Colore preferito
+let favouriteColor = prompt ('Inserisci il tuo colore preferito');
+
+//Random number
+let randomNumber = Math.floor(Math.random() * 100);
+
+//PRINT
+document.writeln( userName + userSurname + favouriteColor + randomNumber)
+console.log (userName + userSurname + favouriteColor + randomNumber)
+
